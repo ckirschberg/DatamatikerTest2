d6131be1987f4a27018f7abba47e48a1d13b2cc3
Christian Kirschberg <ckirschberg@gmail.com>
2015-08-28 08:09:40 +0200

    initial commit

---
+++ src/Horse.java
@@ -0,0 +1,16 @@
+/**
+ * Created by christiankirschberg on 28/08/15.
+ */
+public class Horse {
+    private String name;
+
+    public Horse(String name)
+    {
+        setName(name);
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+}
